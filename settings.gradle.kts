6afb075a22028899bfebf244b402577ad40c6bc9
aiyu-ayaan <aiyuayaan22@gmail.com>
2026-03-03 01:08:30 +0530

    initialize project structure with basic Android setup and theme configuration
    Signed-off-by: aiyu-ayaan <aiyuayaan22@gmail.com>

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AgentVerse"
+include(":app")
+include(":api-integration-common")
+include(":api-integration")
+include(":ui-common")
+include(":agent")
+include(":core")
+include(":data")
